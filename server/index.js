a04d375aac25aa3c2fdc9c9d02d2991d3192949e
sergey <vasilevich_s@i.ua>
2021-04-17 00:35:30 +0300

    test3

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors());
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(express.json({ extended: true }));
 
 startSocket(app);
